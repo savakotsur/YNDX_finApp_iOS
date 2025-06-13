d4f3fc0939014f19a1ce7c799fbb7b2d72c26222
savakotsur <slendygood@gmail.com>
2025-06-13 16:54:04 +0300

    Initial Commit

---
+++ YNDX_finApp_iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YNDX_finApp_iOS
+//
+//  Created by Савелий Коцур on 13.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YNDX_finApp_iOS/YNDX_finApp_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  YNDX_finApp_iOSApp.swift
+//  YNDX_finApp_iOS
+//
+//  Created by Савелий Коцур on 13.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct YNDX_finApp_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
