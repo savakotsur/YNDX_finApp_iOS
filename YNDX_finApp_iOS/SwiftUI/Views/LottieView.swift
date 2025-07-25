edddeba3a512eeddb266325087243a9aab94c853
savakotsur <slendygood@gmail.com>
2025-07-26 02:40:11 +0300

    Доработаны графики, сделано третье задание

---
+++ YNDX_finApp_iOS/SwiftUI/Views/LottieView.swift
@@ -0,0 +1,18 @@
+//
+//  LottieView.swift
+//  YNDX_finApp_iOS
+//
+//  Created by Савелий Коцур on 26.07.2025.
+//
+
+import SwiftUI
+
+struct LottieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LottieView()
+}
